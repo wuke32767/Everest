803b5962c6a963df7af1008783cd0e9fe35bfc71
Popax21 <popa21@popa21.at>
2024-04-14 18:15:16 +0200

    Fix leaked MMR module definition

--- Celeste.Mod.mm/MonoModRules.cs
+++ Celeste.Mod.mm/MonoModRules.cs
@@ -88,6 +88,11 @@ namespace MonoMod {
                     InitModRules(MonoModRule.Modder);
                     break;
             }
+
+            // Null out the rules module reference once we are done patching
+            // Note that MonoMod loads a separate copy of the rules type for each mod we patch (:catresort:)
+            // So this doesn't break anything, however it prevents leaking the rules module definition
+            MonoModRule.Modder.PostProcessors += () => RulesModule = null;
         }
 
         private static void InitCommonRules(MonoModder modder) {
