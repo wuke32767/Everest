fd15d36b301bac3930b8449a7ec68b154903fe76
Vivian <71410564+Viv-0@users.noreply.github.com>
2024-06-19 17:27:13 -0400

    Fixes EntityIDs to be consistent from globally generated members loaded regardless of room started in.

--- Celeste.Mod.mm/Patches/EntityData.cs
+++ Celeste.Mod.mm/Patches/EntityData.cs
@@ -12,5 +12,11 @@ namespace Celeste {
             return orig_Has(key);
         }
 
+        public EntityID EntityID;
+
+        internal void SetEntityID(bool? isTrigger = null) {
+            EntityID = new EntityID(Level.Name ?? string.Empty, ID + ((isTrigger ?? false) || patch_LevelData._isRegisteringTriggers ? 10000000 : 0));
+        }
+
     }
 }
