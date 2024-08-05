a279b91dbd37554335cc823942c9104d11063c82
Maddie <52103563+maddie480@users.noreply.github.com>
2024-08-05 13:55:19 +0200

    Minor formatting tweaks

--- Celeste.Mod.mm/Patches/MiniTextbox.cs
+++ Celeste.Mod.mm/Patches/MiniTextbox.cs
@@ -19,10 +19,10 @@ namespace Celeste {
         private Sprite portrait;
         private FancyText.Portrait portraitData;
         private SoundSource talkerSfx;
-        
+
         private int start;
         private FancyText.Anchors anchor;
-        
+
         public patch_MiniTextbox(string dialogId)
             : base(dialogId) {
             // no-op. MonoMod ignores this - we only need this to make the compiler shut up.
@@ -32,7 +32,7 @@ namespace Celeste {
         [MonoModConstructor]
         public void ctor(string dialogId) {
             orig_ctor(dialogId);
-            
+
             // Find the anchor
             foreach (FancyText.Node node in text.Nodes) {
                 if (node is FancyText.Anchor anchorPos) {
@@ -44,7 +44,7 @@ namespace Celeste {
         [MonoModIgnore]
         [PatchMiniTextboxRoutine]
         private extern IEnumerator Routine();
-        
+
         [MonoModIgnore]
         [PatchMiniTextboxRender]
         public override extern void Render();
@@ -57,25 +57,23 @@ namespace Celeste {
                 portrait.Play(portraitData.TalkAnimation);
             }
         }
-        
+
         private void _handleDialogNode(ref float delay) {
             if (text[index] is FancyText.Wait wait) {
                 delay += wait.Duration;
             } else if (text[index] is FancyText.NewPage) {
                 start = index + 1;
             }
-            
-            if (delay > 0.5f)
-            {
+
+            if (delay > 0.5f) {
                 talkerSfx?.Param("dialogue_portrait", 0f);
                 talkerSfx?.Param("dialogue_end", 1f);
-                if (portrait != null && portraitData != null && portrait.Has(portraitData.IdleAnimation) && portrait.CurrentAnimationID != portraitData.IdleAnimation)
-                {
+                if (portrait != null && portraitData != null && portrait.Has(portraitData.IdleAnimation) && portrait.CurrentAnimationID != portraitData.IdleAnimation) {
                     portrait.Play(portraitData.IdleAnimation);
                 }
             }
         }
-        
+
         private void _applyAnchor(ref Vector2 center) {
             if (anchor == FancyText.Anchors.Bottom) {
                 center = new Vector2(Engine.Width / 2, Engine.Height - BoxHeight / 2.0f - (Engine.Width - BoxWidth) / 4f);
@@ -93,7 +91,7 @@ namespace MonoMod {
     /// </summary>
     [MonoModCustomMethodAttribute(nameof(MonoModRules.PatchMiniTextboxRoutine))]
     class PatchMiniTextboxRoutine : Attribute { }
-    
+
     [MonoModCustomMethodAttribute(nameof(MonoModRules.PatchMiniTextboxRender))]
     class PatchMiniTextboxRender : Attribute { }
 
@@ -102,7 +100,7 @@ namespace MonoMod {
         public static void PatchMiniTextboxRoutine(MethodDefinition method, CustomAttribute attrib) {
             MethodDefinition m_MiniTextbox_startTalking = method.DeclaringType.FindMethod("_startTalking")!;
             MethodDefinition m_MiniTextbox_handleNode = method.DeclaringType.FindMethod("_handleDialogNode")!;
-            
+
             FieldDefinition f_MiniTextbox_closing = method.DeclaringType.FindField("closing")!;
 
             // The routine is stored in a compiler-generated method.
@@ -148,44 +146,44 @@ namespace MonoMod {
                 cursor.EmitRet();
                 cursor.MarkLabel(yieldReturnNullTarget);
             });
-            
+
             new ILContext(method).Invoke(il => {
                 ILCursor cursor = new(il);
-                
+
                 // Before: 'if (text.Nodes[index] is FancyText.Char) { ... }' 
                 cursor.GotoNext(MoveType.AfterLabel, instr => instr.MatchIsinst("Celeste.FancyText/Char"));
                 cursor.GotoPrev(MoveType.AfterLabel, instr => instr.MatchLdloc1());
                 cursor.GotoPrev(MoveType.AfterLabel, instr => instr.MatchLdloc1());
-                
+
                 cursor.EmitLdloc1(); // this
                 cursor.EmitCall(m_MiniTextbox_startTalking);
-                
+
                 // Before: 'index++;'
                 cursor.GotoNext(MoveType.AfterLabel,
                     instr => instr.MatchLdloc1(),
                     instr => instr.MatchLdloc1(),
                     instr => instr.MatchLdfld("Celeste.MiniTextbox", "index"));
-                
+
                 cursor.EmitLdloc1(); // this
                 cursor.EmitLdloca(3); // ref float delay
                 cursor.EmitCall(m_MiniTextbox_handleNode);
             });
         }
-        
+
         public static void PatchMiniTextboxRender(ILContext il, CustomAttribute attrib) {
             MethodDefinition m_MiniTextbox_applyAnchor = il.Method.DeclaringType.FindMethod("_applyAnchor")!;
-            
+
             FieldDefinition f_MiniTextbox_start = il.Method.DeclaringType.FindField("start")!;
-            
+
             ILCursor cursor = new(il);
-            
+
             // After: 'Vector2 center = new Vector2(Engine.Width / 2, BoxHeight / 2.0f + (Engine.Width - BoxWidth) / 4f);'
             cursor.GotoNext(MoveType.After, instr => instr.MatchCall("Microsoft.Xna.Framework.Vector2", ".ctor"));
-            
+
             cursor.EmitLdarg0();
             cursor.EmitLdloca(1); // ref Vector2 center
             cursor.EmitCall(m_MiniTextbox_applyAnchor);
-            
+
             // Replace: 'text.Draw(new Vector2(topleft.X + portraitSize + 32f, center.Y), new Vector2(0f, 0.5f), new Vector2(1f, ease) * 0.75f, 1f, 0, index);'
             // With:    'text.Draw(new Vector2(topleft.X + portraitSize + 32f, center.Y), new Vector2(0f, 0.5f), new Vector2(1f, ease) * 0.75f, 1f, start, index);'
             cursor.Index = il.Instrs.Count - 1;
