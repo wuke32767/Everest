643fff5abc5452531fce1aa0f9ee3cf7c5b9ce87
microlith57 <microlith57@gmail.com>
2024-08-14 19:28:19 +1200

    remove unnecessary warning suppression

--- Celeste.Mod.mm/Patches/Spikes.cs
+++ Celeste.Mod.mm/Patches/Spikes.cs
@@ -1,6 +1,4 @@
-﻿#pragma warning disable CS0626 // Method, operator, or accessor is marked external and has no attributes on it
-
-using Celeste.Mod.Helpers;
+﻿using Celeste.Mod.Helpers;
 using Microsoft.Xna.Framework;
 using Mono.Cecil;
 using Mono.Cecil.Cil;
