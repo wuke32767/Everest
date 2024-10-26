20ad78a4a7f9d21ea62b0de218377c8539221f38
Gamation <52021489+GamationOnGithub@users.noreply.github.com>
2024-10-25 19:19:16 -0700

    Add functionality for Distort suppression
    No IL patch here thank god

---
+++ Celeste.Mod.mm/Patches/Distort.cs
@@ -0,0 +1,22 @@
+﻿using Celeste.Mod.Core;
+using MonoMod;
+
+namespace Celeste {
+    class patch_Distort {
+
+        private static float anxiety = 0f;
+        
+        [MonoModReplace]
+        public new static float Anxiety {
+            get 
+            {
+                return anxiety;
+            }
+            set 
+            {
+                anxiety = value;
+                GFX.FxDistort.Parameters["anxiety"].SetValue((CoreModule.Settings.AllowDistort) ? anxiety : 0f);
+            } 
+        }
+    }
+}
